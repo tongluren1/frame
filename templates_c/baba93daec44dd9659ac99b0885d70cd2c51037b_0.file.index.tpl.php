14e3709b676db16751e2871dc950c1cca7383208
guozheng wang <30545425+tongluren1@users.noreply.github.com>
2020-07-08 15:45:07 +0800

    Update baba93daec44dd9659ac99b0885d70cd2c51037b_0.file.index.tpl.php

--- templates_c/baba93daec44dd9659ac99b0885d70cd2c51037b_0.file.index.tpl.php
+++ templates_c/baba93daec44dd9659ac99b0885d70cd2c51037b_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2019-10-21 08:31:08
+/* Smarty version 3.1.33, created on 2020-07-08 06:27:37
   from 'G:\test\frame\app\view\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5dad6cccd83b83_47410177',
+  'unifunc' => 'content_5f0567592c7a89_59728935',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'baba93daec44dd9659ac99b0885d70cd2c51037b' => 
     array (
       0 => 'G:\\test\\frame\\app\\view\\index.tpl',
-      1 => 1571645142,
+      1 => 1571647034,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5dad6cccd83b83_47410177 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f0567592c7a89_59728935 (Smarty_Internal_Template $_smarty_tpl) {
 ?>hello <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
  !<?php }
 }
